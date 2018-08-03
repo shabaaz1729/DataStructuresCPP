2b3d32e00c057a3878abaeac43bf56e65878773f
shabaaaz1729 <shabaaz.nita@gmail.com>
2018-08-04 03:44:46 +0530

    stacks

---
+++ Stacks/EvaluationOfPrefix.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int cal(int x,int y,char c)
+{
+	if(c=='+')
+		return x+y;
+	else if(c=='-')
+		return x-y;
+	else if(c=='*')
+		return x*y;
+	else if(c=='/')
+		return x/y;
+}
+
+int evaluate(string str)
+{
+	stack<int> S;
+	for(int i=str.length()-1;i>=0;i--)
+	{
+		if(isdigit(str[i]))
+		{
+			int x=str[i]-'0';
+			S.push(x);
+		}
+		else
+		{
+			int op1=S.top(); S.pop();
+			int op2=S.top(); S.pop();
+			int res=cal(op1,op2,str[i]);
+			S.push(res);
+		}
+	}
+
+	return S.top();
+}
+
+int main()
+{
+	string s="-+*12*345";
+	int val=evaluate(s);
+	cout<<val<<endl;
+	return 0;
+}
