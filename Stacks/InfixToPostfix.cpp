21811fe61461ddea7bf7a512eb8656a3090d56ad
shabaaaz1729 <shabaaz.nita@gmail.com>
2018-08-05 11:00:20 +0530

    infix to postfix

--- Stacks/InfixToPostfix.cpp
+++ Stacks/InfixToPostfix.cpp
@@ -1,53 +1,69 @@
-#include<iostream>
-#include<string>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
 
-bool Gprec(char c,char d)
+bool Gprece(char c,char d)
 {
-	if((d=='-' || d=='+') && (c=='*' || c=='/'))
+    if((c=='*' || c=='/' || c=='^') && (d=='-' || d=='+'))
 		return true;
 	else if((c=='-' || c=='+') && (d=='-' || d=='+'))
 		return true;
 	else if((c=='*' || c=='/') && (d=='*' || d=='/'))
 		return true;
+	else if((c=='^') && (d=='*' || d=='/' || d=='^'))
+	    return true;
 	else
-		return false;
-}	
+		return false;    
+}
 
-int main()
-{
-	string str="2+3*4-5*6";
-	string d="";
-	stack<char> S;
-	for(int i=0;i<str.length();i++)
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
 	{
-		if(isdigit(str[i]))
-			d+=str[i];
-		else
-		{
-			if(S.empty())
-				S.push(str[i]);
-			else
-			{
-				while(Gprec(S.top(),str[i]))
-				{
-					d+=S.top();
-					S.pop();	
-					if(S.empty())
-						break;
-				}
-
-				S.push(str[i]);
-			}
-		}
+	    string s;
+	    cin>>s;
+	    string d="";
+	    stack<char> S;
+	    for(int i=0;i<s.length();i++)
+        {
+            if(isalpha(s[i]))
+                d+=s[i];
+            
+            else if(s[i]=='*' || s[i]=='+' || s[i]=='-' || s[i]=='/' || s[i]=='^')
+            {
+                while(!S.empty() && S.top()!='(' && Gprece(S.top(),s[i]))
+                {
+                    d+=S.top();
+                    S.pop();
+                }
+                    S.push(s[i]);
+            }
+            
+            else if(s[i]=='(')
+                S.push(s[i]);
+            
+            else if(s[i]==')')
+            {
+                while(!S.empty() && S.top()!='(')
+                {
+                    d+=S.top();
+                    S.pop();
+                }
+                
+                S.pop();
+            }
+        }
+        
+        while(!S.empty())
+        {
+            d+=S.top();
+            S.pop();
+        }
+        
+        cout<<d<<endl;
 	}
-	while(!S.empty())
-	{
-		d+=S.top();
-		S.pop();
-	}
-
-	cout<<d<<endl;
 	return 0;
 }
