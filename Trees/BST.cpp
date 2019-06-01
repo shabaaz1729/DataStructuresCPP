14b84896e77724592185c5292f566fdb4d6dcbd5
shabaaaz1729 <shabaaz.nita@gmail.com>
2019-06-01 17:27:51 +0530

    BST Implementation

--- Trees/BST.cpp
+++ Trees/BST.cpp
@@ -1,99 +1,104 @@
-#include <iostream>
+#include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct BstNode
 {
 	int data;
-	BstNode* left;
-	BstNode* right;
+	BstNode* lc;
+	BstNode* rc;
 };
 
-BstNode* insert(BstNode* root,int n)
+BstNode* root = NULL;
+
+BstNode* insert(BstNode* root, int d)
 {
 	if(root==NULL)
 	{
-		BstNode* temp=new BstNode();
-		temp->data=n;
-		temp->left=NULL;
-		temp->right=NULL;
-		return temp;
+		BstNode* temp = new BstNode();
+		temp->data = d;
+		temp->lc = NULL;
+		temp->rc = NULL;
+		root = temp;
 	}
 
-	else if(n<=root->data)
-	{
-		root->left=insert(root->left,n);
-		return root;
-	}
+	else if(d <= root->data)
+		root->lc = insert(root->lc,d);
+
 	else
-	{
-		root->right=insert(root->right,n);
-		return root;
-	}
+		root->rc = insert(root->rc,d);
 
+	return root;
 }
 
-bool search(BstNode* root,int d)
+bool isFound(BstNode* root, int d)
 {
-	if(root==NULL)
+	if(root == NULL)
 		return false;
 
-	else if(d==root->data)
+	else if(root->data == d)
 		return true;
 
-	else if(d<=root->data)
-		return search(root->left,d);
-
+	else if(d < root->data)
+		return isFound(root->lc, d);
+	
 	else 
-		return search(root->right,d);
-
-	return false;
+		return isFound(root->rc, d);
 }
 
 int findMin(BstNode* root)
 {
-	if(root==NULL)
-		return -1;
-	while(root->left!=NULL)
-		root=root->left;
+	if(root->lc == NULL)
+		return root->data;
 
-	return root->data;
+	return findMin(root->lc);
 }
 
 int findMax(BstNode* root)
 {
-	if(root==NULL)
-		return -1;
-	while(root->right!=NULL)
-		root=root->right;
+	if(root->rc == NULL)
+		return root->data;
 
-	return root->data;
+	return findMax(root->rc);
 }
 
+int findHeight(BstNode* root)
+{
+	if(root==NULL)
+		return -1;
+
+	return max(findHeight(root->lc),findHeight(root->rc))+1 ;
+}
 
 int main()
 {
-	BstNode* root=NULL;
-	root=insert(root,12);
-	root=insert(root,34);
-	root=insert(root,25);
-	root=insert(root,11);
-	root=insert(root,13);
-	root=insert(root,9);
-	root=insert(root,22);
+	root = insert(root,50);
+	root = insert(root,76);
+	root = insert(root,21);
+	root = insert(root,4);
+	root = insert(root,32);
+	root = insert(root,64);
+	root = insert(root,15);
+	root = insert(root,52);
+	root = insert(root,14);
+	root = insert(root,100);
+	root = insert(root,83);
+	root = insert(root,2);
+	root = insert(root,3);
+	root = insert(root,70);
+	root = insert(root,87);
+	root = insert(root,80);
 
-	cout<<"Enter a number to search\n";
-	int n;
-	cin>>n;
+	if(isFound(root,200))
+		cout<<"87 is found\n";
 
-	if(search(root,n))
-		cout<<"Number Found\n";
-	else
-		cout<<"Number not Found\n";
+	if(!isFound(root,4))
+		cout<<"4 is not found\n";
 
-	cout<<"Minimum number in the tree is "<<findMin(root)<<endl;
-	
-	cout<<"Maximum number in the tree is "<<findMax(root)<<endl;
+	cout<<"Min element in the tree is "<<findMin(root)<<endl;
+	cout<<"Max element in the tree is "<<findMax(root)<<endl;
 
+	cout<<"Height of the tree is "<<findHeight(root)<<endl;
 
-	return 0;
-}	
+  	return 0;
+}
